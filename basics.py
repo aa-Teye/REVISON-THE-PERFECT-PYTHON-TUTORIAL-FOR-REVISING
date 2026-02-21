b64e69d54adea4d8b70f79fb55ff71e840af77c4
aa-Teye <alexteyeametepey@gmail.com>
2026-02-21 23:15:13 +0000

    Third Commit

--- basics.py
+++ basics.py
@@ -10,3 +10,7 @@ name, level, is__active = "Alex", 200, True
 
 # A remind that the input function always returns a string, no matter what the user types.
 age_input = input("Please enter your age: ")
+
+# Doing maths with it, so therefore we need to convert it to an integer first.
+age_next_five_years = int(age_input) + 5
+print(f"By the next five years, {name} will be {age_next_five_years} years old.")
