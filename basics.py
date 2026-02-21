d7ab152ce3452c9a9ec554c52604411f9868173f
aa-Teye <alexteyeametepey@gmail.com>
2026-02-21 23:05:44 +0000

    first commit

---
+++ basics.py
@@ -0,0 +1,9 @@
+identity = "Alex" # A string
+identity  = 10977320 # An integer
+
+#Learning how python show different data types
+# print(f"current identity: {identity} | type: {type(identity)}")
+
+
+#Trying the multiple assignment role
+name, level, is__active = "Alex", 200, True
